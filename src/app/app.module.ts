eb19a72d4a85e70e735e465323a9c054b3babb97
John Diatte <jcdiatte@gmail.com>
2020-11-02 21:32:35 -0600

    hw3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { RequestComponentComponent } from './request-component/request-component.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {path: 'request-component', component:RequestComponentComponent},
+  {path: 'about', component:AboutComponent},
+  {path: '', redirectTo: '/request-component', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    RequestComponentComponent
+    RequestComponentComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
